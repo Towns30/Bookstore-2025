ece099ce5a4c6dfe39b8ec2b5a2394e61ed92ead
Towns <tangzheyu@sjtu.edu.cn>
2025-12-21 21:04:20 +0800

    feat: Statement

--- include/Statement.hpp
+++ include/Statement.hpp
@@ -5,9 +5,166 @@
 class Statement
 {
 public:
-  Statement();
+  Statement() {};
   ~Statement() = default;
-  virtual void Execute(std::string user_id);
+  virtual void Execute() = 0;
+};
+class SuStatement : public Statement
+{
+public:
+  void Execute();
+  SuStatement(std::string user_id, std::string passwd) : user_id_(user_id), passwd_(passwd) {}
+
+private:
+  std::string user_id_;
+  std::string passwd_;
+};
+class LogoutStatement : public Statement
+{
+public:
+  void Execute();
+  LogoutStatement() {}
+};
+class RegisterStatement : public Statement
+{
+public:
+  void Execute();
+  RegisterStatement(std::string user_id, std::string passwd, std::string user_name) : user_id_(user_id), passwd_(passwd), user_name_(user_name){}
+
+private:
+  std::string user_id_;
+  std::string passwd_;
+  std::string user_name_;
+};
+class PasswdStatement : public Statement
+{
+public:
+  void Execute();
+  PasswdStatement(std::string user_id, std::string cur_passwd, std::string new_passwd)
+      : user_id_(user_id), cur_passwd_(cur_passwd), new_passwd_(new_passwd)
+  {
+  }
+
+private:
+  std::string user_id_;
+  std::string cur_passwd_;
+  std::string new_passwd_;
+};
+class UserAddStatement : public Statement
+{
+public:
+  void Execute();
+  UserAddStatement(std::string user_id, std::string passwd, int privilege, std::string user_name)
+      : user_id_(user_id), passwd_(passwd), privilege_(privilege), user_name_(user_name)
+  {
+  }
+
+private:
+  std::string user_id_;
+  std::string passwd_;
+  int privilege_;
+  std::string user_name_;
+};
+class DeleteStatement : public Statement
+{
+public:
+  void Execute();
+  DeleteStatement(std::string user_id) : user_id_(user_id) {}
+
+private:
+  std::string user_id_;
+};
+class ShowBookStatement : public Statement
+{
+public:
+  void Execute();
+  ShowBookStatement(std::string isbn, std::string bookname, std::string author, std::string keyword,
+                    bool all_show = false)
+      : isbn_(isbn), bookname_(bookname), author_(author), keyword_(keyword), all_show_(all_show)
+  {
+  }
+
+private:
+  std::string isbn_;
+  std::string bookname_;
+  std::string author_;
+  std::string keyword_;
+  bool all_show_;
+};
+class BuyStatement : public Statement
+{
+public:
+  void Execute();
+  BuyStatement(std::string isbn, int quantity) : isbn_(isbn), quantity_(quantity) {}
+
+private:
+  std::string isbn_;
+  int quantity_;
+};
+class SelectStatement : public Statement
+{
+public:
+  void Execute();
+  SelectStatement(std::string isbn) : isbn_(isbn) {}
+
+private:
+  std::string isbn_;
+};
+class ModifyStatement : public Statement
+{
+public:
+  void Execute();
+  ModifyStatement(std::string isbn, std::string bookname, std::string author, std::string keyword,
+                  long long price = -1)
+      : isbn_(isbn), bookname_(bookname), author_(author), keyword_(keyword), price_(price)
+  {
+  }
+
+private:
+  std::string isbn_;
+  std::string bookname_;
+  std::string author_;
+  std::string keyword_;
+  long long price_; // price * 100
+};
+class ImportStatement : public Statement
+{
+public:
+  void Execute();
+  ImportStatement(int quantity, long long total_cost) : quantity_(quantity), total_cost_(total_cost)
+  {
+  }
+
+private:
+  int quantity_;
+  long long total_cost_; // totalcost * 100
+};
+class ShowFinanceStatement : public Statement
+{
+public:
+  void Execute();
+  ShowFinanceStatement(int count = -1) : count_(count) {}
+
+private:
+  int count_;
+};
+class LogStatement : public Statement
+{
+public:
+  void Execute();
+  LogStatement() {}
+};
+class ReportFinanceStatement : public Statement
+{
+public:
+  void Execute();
+  ReportFinanceStatement() {}
+};
+class ReportEmployeeStatement : public Statement
+{
+public:
+  void Execute();
+  ReportEmployeeStatement() {}
 };
 
 #endif
---
+++ src/Statement.cpp
@@ -0,0 +1,149 @@
+#include "../include/Statement.hpp"
+#include "../include/Managers.hpp"
+
+
+void SuStatement::Execute() { AccountManager::getInstance().Su(user_id_, passwd_); }
+void LogoutStatement::Execute()
+{
+  if (AccountManager::getInstance().GetCurrentPrivilege() < 1) // 权限检查
+  {
+    throw std::runtime_error("Invalid\n");
+  }
+  AccountManager::getInstance().Logout();
+}
+void RegisterStatement::Execute()
+{
+  AccountManager::getInstance().Register(user_id_, passwd_, user_name_);
+}
+void PasswdStatement::Execute()
+{
+  if (AccountManager::getInstance().GetCurrentPrivilege() < 1) // 权限检查
+  {
+    throw std::runtime_error("Invalid\n");
+  }
+  AccountManager::getInstance().Passwd(user_id_, new_passwd_, cur_passwd_);
+}
+void UserAddStatement::Execute()
+{
+  if (AccountManager::getInstance().GetCurrentPrivilege() < 3) // 权限检查
+  {
+    throw std::runtime_error("Invalid\n");
+  }
+  AccountManager::getInstance().UserAdd(user_id_, passwd_, privilege_, user_name_);
+}
+void DeleteStatement::Execute()
+{
+  if (AccountManager::getInstance().GetCurrentPrivilege() < 7) // 权限检查
+  {
+    throw std::runtime_error("Invalid\n");
+  }
+  AccountManager::getInstance().Delete(user_id_);
+}
+void ShowBookStatement::Execute()
+{
+  if (AccountManager::getInstance().GetCurrentPrivilege() < 1) // 权限检查
+  {
+    throw std::runtime_error("Invalid\n");
+  }
+  BookShowPackage book_show_package;
+  if(all_show_)
+  {
+    book_show_package.type_ = BookShowPackage::ShowType::ALL;
+  }
+  else if(!isbn_.empty())
+  {
+    book_show_package.type_ = BookShowPackage::ShowType::ISBN;
+    book_show_package.value_ = isbn_;
+  }
+  else if(!bookname_.empty())
+  {
+    book_show_package.type_ = BookShowPackage::ShowType::BOOKNAME;
+    book_show_package.value_ = bookname_;
+  }
+  else if(!author_.empty())
+  {
+    book_show_package.type_ = BookShowPackage::ShowType::AUTHOR;
+    book_show_package.value_ = author_;
+  }
+  else if(!keyword_.empty())
+  {
+    book_show_package.type_ = BookShowPackage::ShowType::KEYWORD;
+    book_show_package.value_ = keyword_;
+  }
+  else
+  {
+    throw std::runtime_error("Invalid\n");
+  }
+  BookManager::getInstance().Show(book_show_package);
+}
+void BuyStatement::Execute()
+{
+  if (AccountManager::getInstance().GetCurrentPrivilege() < 1) // 权限检查
+  {
+    throw std::runtime_error("Invalid\n");
+  }
+  BookManager::getInstance().Buy(isbn_, quantity_);
+}
+void SelectStatement::Execute()
+{
+  if (AccountManager::getInstance().GetCurrentPrivilege() < 3) // 权限检查
+  {
+    throw std::runtime_error("Invalid\n");
+  }
+  BookManager::getInstance().Select(isbn_);
+}
+void ModifyStatement::Execute()
+{
+  if (AccountManager::getInstance().GetCurrentPrivilege() < 3) // 权限检查
+  {
+    throw std::runtime_error("Invalid\n");
+  }
+  BookModifyPackage book_modify_package;
+  if (!this->isbn_.empty())
+  {
+    book_modify_package.isbn_ = this->isbn_;
+  }
+  if (!this->bookname_.empty())
+  {
+    book_modify_package.bookname_ = this->bookname_;
+  }
+  if (!this->author_.empty())
+  {
+    book_modify_package.author_ = this->author_;
+  }
+  if (!this->keyword_.empty())
+  {
+    book_modify_package.keyword_ = this->keyword_;
+  }
+  if (this->price_ != -1)
+  {
+    book_modify_package.price_ = this->price_;
+  }
+  BookManager::getInstance().Modify(book_modify_package);
+}
+void ImportStatement::Execute()
+{
+  if (AccountManager::getInstance().GetCurrentPrivilege() < 3) // 权限检查
+  {
+    throw std::runtime_error("Invalid\n");
+  }
+  BookManager::getInstance().Import(quantity_, total_cost_);
+}
+void ShowFinanceStatement::Execute()
+{
+  if (AccountManager::getInstance().GetCurrentPrivilege() < 7) // 权限检查
+  {
+    throw std::runtime_error("Invalid\n");
+  }
+  if (count_ == -1)
+  {
+    FinanceManager::getInstance().ShowAllFinanceReport();
+    return;
+  }
+  else
+  {
+    FinanceManager::getInstance().ShowFinanceReport(count_);
+  }
+}
+void LogStatement::Execute() {}
+void ReportFinanceStatement::Execute() {}
